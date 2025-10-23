3de3cdb3643670cd8d51fb78e869d627d9581f25
Pranesh <praneshmadhan646@gmail.com>
2025-10-23 13:29:52 +0530

    enchanced system prompt

--- backend/agents/summary_agent.py
+++ backend/agents/summary_agent.py
@@ -9,7 +9,7 @@ def summarize_text(text: str):
     data = {
         "model": "z-ai/glm-4.5-air:free",  # free model
         "messages": [
-            {"role": "system", "content": "You are a helpful research summarizer."},
+            {"role": "system", "content": "You are a helpful research summarizer. Refer the data given by the user and use your own knowledge to generate a concise summary. Refer Research papers, articles, and verified sources which is in your training data or memory, But don't make it hard for the user to understand keep it simple and easy to read/understand."},
             {"role": "user", "content": f"Summarize these findings into clear points:\n\n{text}"}
         ]
     }
